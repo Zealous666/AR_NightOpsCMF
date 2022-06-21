7a44cec35624798d9da4dc55ba436147d4797827
Daniel Lewis <d.lewis.1991@googlemail.com>
2022-06-21 22:35:28 +0100

    Satchel charge mechanic added
    Can be applied to various objects.
    Action can be added to said objects to trigger a set timer.
    Includes UI countdown then boom!

--- Scripts/Game/NO_CoopMissionsFramework/DCP/NO_SCR_DCP_SabotageDestr_Action.c
+++ Scripts/Game/NO_CoopMissionsFramework/DCP/NO_SCR_DCP_SabotageDestr_Action.c
@@ -1,30 +1,50 @@
 class NO_SCR_DCP_SabotageDestr_Action : NO_SCR_OneTimeAction
 {
-	
+	[Attribute("40", UIWidgets.Slider, desc: "Timer for big bada boom [s]", params: "0 90 1")]
+	protected int m_iTimer;
+
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
-		{
-		
-			super.PerformAction(pOwnerEntity, pUserEntity);
-		
-		//Show popup
-		SCR_PopUpNotification.GetInstance().PopupMsg("SATCHEL CHARGE PLANTED", duration: 5);
-		
-		//Show hint
-		SCR_HintManagerComponent hintComponent = SCR_HintManagerComponent.GetInstance();
-		hintComponent.ShowCustomHint("40 Seconds till satchel explodes", "Satchels placed", 15);
-		
- 		//Set delay-timer to destroy
-    	GetGame().GetCallqueue().CallLater(DelayedDestruction, 10000, false);
-		
-	}
-
-	//Destroy after delay
-	protected void DelayedDestruction()
 	{
-		
-    SCR_PopUpNotification.GetInstance().PopupMsg("BOOM", duration: 60);
-	//TO-DO Destroy/Explode pOwnerEntity
-	
+		// Required for one time action
+		super.PerformAction(pOwnerEntity, pUserEntity);
+
+		// Show popup
+		SCR_PopUpNotification.GetInstance().PopupMsg("SATCHEL CHARGE PLANTED", duration: 5);
+
+ 		// Start countdown (repeating fn call)
+    	GetGame().GetCallqueue().CallLater(Countdown, 1000, true);
+		ShowCountdown();
 	}
 
+	// Show GUI hint with time remaining
+	protected void ShowCountdown()
+	{
+		SCR_HintManagerComponent.GetInstance().ShowCustomHint(string.Format("%1 seconds until satchel explodes!", m_iTimer), "Satchel Placed", 3);
+	}
+
+	// Countdown function
+	protected void Countdown()
+	{
+		// Decrement coundown
+		m_iTimer--;
+
+		ShowCountdown();
+
+		if (m_iTimer < 1)
+		{
+			// Remove repeating countdown function
+			GetGame().GetCallqueue().Remove(Countdown);
+
+			// Only authority should deystroy the vehicle
+			if (RplSession.Mode() != RplMode.Client)
+				DestroyVehicle();
+		}
+	}
+
+	// Trigger BaseTriggerComponent
+	protected void DestroyVehicle()
+	{
+		BaseTriggerComponent trigger = BaseTriggerComponent.Cast(GetOwner().FindComponent(BaseTriggerComponent));
+		trigger.OnUserTrigger(GetOwner());
+	}
 }
