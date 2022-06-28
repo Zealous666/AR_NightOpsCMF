cf8c13af8d3d2a5b400f2ebbaaa968b9bd450f6d
Daniel Lewis <d.lewis.1991@googlemail.com>
2022-06-29 00:37:52 +0100

    Audio on actions setup

--- Scripts/Game/NO_CoopMissionsFramework/DCP/TypeIntel/NO_SCR_DCP_IntelDownload_Action.c
+++ Scripts/Game/NO_CoopMissionsFramework/DCP/TypeIntel/NO_SCR_DCP_IntelDownload_Action.c
@@ -1,15 +1,75 @@
 class NO_SCR_DCP_IntelDownload_Action : NO_SCR_OneTimeAction
 {
-	
+	[Attribute("SOUND_RADIO_CHATTER_RU", UIWidgets.ComboBox, desc: "", enums: { new ParamEnum("SOUND_RADIO_CHATTER_RU", "SOUND_RADIO_CHATTER_RU"), new ParamEnum("SOUND_RADIO_CHATTER_US", "SOUND_RADIO_CHATTER_US"), new ParamEnum("SOUND_RADIO_CHATTER_EV", "SOUND_RADIO_CHATTER_EV") })]
+	protected string m_sRadioChatter;
+
+	protected AudioHandle m_pAudioHandle;
+
+
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
 	{
+		// Required for one time action
 		super.PerformAction(pOwnerEntity, pUserEntity);
-		
-		//Finish pickup intel task
+
+		// Get intel task
 		IEntity taskEntity = GetGame().GetWorld().FindEntityByName("IntelPatrol_2_US");
+		if (!taskEntity) return;
         NO_SCR_EditorTask task = NO_SCR_EditorTask.Cast(taskEntity);
+		if (!task) return;
+
+		// Finish intel task
 		task.ChangeStateOfTask(TriggerType.Finish);
-	
+
+		// Play the set faction chatter (3D)
+		PlaySound(m_sRadioChatter);
 	}
-	
+
+
+	// Taken from toilet/piano example
+	protected void PlaySound(string sound)
+	{
+		GameSignalsManager globalSignalsManager = GetGame().GetSignalsManager();
+
+		ref array<string> signalName = new array<string>;
+		ref array<float> signalValue = new array<float>;
+
+		signalName.Insert("GInterior");
+		signalName.Insert("GIsThirdPersonCam");
+		signalName.Insert("GCurrVehicleCoverage");
+
+		foreach(string signal : signalName)
+			signalValue.Insert(globalSignalsManager.GetSignalValue(globalSignalsManager.AddOrFindSignal(signal)));
+
+		vector mat[4];
+		mat[3] = GetOwner().GetOrigin();
+
+		if (m_pAudioHandle)
+			AudioSystem.TerminateSound(m_pAudioHandle);
+
+		m_pAudioHandle = AudioSystem.PlayEvent("{4340FAB152ACD12D}Sounds/UI/UI_Radio_Establish_Action.acp", sound, mat, signalName, signalValue);
+	}
+
+
+	// ------------------------------------------------------------ //
+	// Plays radio establish noise for the user while in the action //
+	// ------------------------------------------------------------ //
+	override void OnActionStart(IEntity pUserEntity)
+	{
+		super.OnActionStart(pUserEntity);
+		PlaySound("SOUND_RADIO_ESTABLISH_ACTION");
+	}
+
+	override void OnActionCanceled(IEntity pOwnerEntity, IEntity pUserEntity)
+	{
+		if (m_pAudioHandle)
+			AudioSystem.TerminateSound(m_pAudioHandle);
+	}
+
+	// ---------------------------------------------------------------
+
+
+	void ~NO_SCR_DCP_IntelDownload_Action()
+	{
+		AudioSystem.TerminateSound(m_pAudioHandle);
+	}
 }
--- Scripts/Game/NO_CoopMissionsFramework/DCP/TypeIntel/NO_SCR_DCP_IntelPickup_Action.c
+++ Scripts/Game/NO_CoopMissionsFramework/DCP/TypeIntel/NO_SCR_DCP_IntelPickup_Action.c
@@ -1,16 +1,41 @@
 class NO_SCR_DCP_IntelPickup_Action : ScriptedUserAction
 {
-	
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
 	{
-		//Finish pickup intel task
+		// Get intel task
 		IEntity taskEntity = GetGame().GetWorld().FindEntityByName("IntelPatrol_1_US");
+		if (!taskEntity) return;
         NO_SCR_EditorTask task = NO_SCR_EditorTask.Cast(taskEntity);
+		if (!task) return;
+
+		// Finish pickup intel task
 		task.ChangeStateOfTask(TriggerType.Finish);
-	
-		
-		//Removes intel piece from world
+
+		// Play the map pickup sound (3D)
+		PlaySound();
+
+		// Removes intel piece from world
 		SCR_EntityHelper.DeleteEntityAndChildren(pOwnerEntity);
 	}
-	
+
+	// Taken from toilet/piano example
+	protected void PlaySound()
+	{
+		GameSignalsManager globalSignalsManager = GetGame().GetSignalsManager();
+
+		ref array<string> signalName = new array<string>;
+		ref array<float> signalValue = new array<float>;
+
+		signalName.Insert("GInterior");
+		signalName.Insert("GIsThirdPersonCam");
+		signalName.Insert("GCurrVehicleCoverage");
+
+		foreach(string signal : signalName)
+			signalValue.Insert(globalSignalsManager.GetSignalValue(globalSignalsManager.AddOrFindSignal(signal)));
+
+		vector mat[4];
+		mat[3] = GetOwner().GetOrigin();
+
+		AudioSystem.PlayEvent("{13F8D0DE67678118}Sounds/Items/_SharedData/PickUp/Items_PickUp_Map.acp", "SOUND_PICK_UP", mat, signalName, signalValue);
+	}
 }
