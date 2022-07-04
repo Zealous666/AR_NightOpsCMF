94960e2e714a8feb54bb9bee306e32b564be8318
Daniel Lewis <d.lewis.1991@googlemail.com>
2022-07-05 00:31:58 +0100

    Redesigned spawnpoint deployments
    This was due to issues with spawnpoints as children entities, when spawned in runtime rather than statically placed.
    Also updated the example level to show these changes correctly with a runtime spawned UAZ.

--- Scripts/Game/NO_CoopMissionsFramework/Common/Deployments/NO_SCR_SpawnpointDeployment.c
+++ Scripts/Game/NO_CoopMissionsFramework/Common/Deployments/NO_SCR_SpawnpointDeployment.c
@@ -1,26 +1,52 @@
 [BaseContainerProps()]
 class NO_SCR_SpawnpointDeployment : NO_SCR_DeploymentInterface
 {
-	[Attribute(defvalue: "", UIWidgets.EditBox, desc: "Key to set the spawnpoint to on deployment")]
-	protected FactionKey m_sFactionKey;
+	[Attribute(desc: "Spawnpoint offset from deployable entity, to avoid being inside it.", params: "inf inf 0 purposeCoords spaceEntity")]
+	protected vector m_vSpawnpointOffset;
+
+	//-----------------------------------------------------------------------
+	// Spawnpoint attributes as seen in SCR_SpawnPoint
+	//-----------------------------------------------------------------------
+	[Attribute("0", desc: "Find empty position for spawning within given radius. When none is found, entity position will be used.")]
+	protected float m_fSpawnRadius;
+
+	[Attribute("2", UIWidgets.EditBox, "Determines how close a player has to be to disable this spawn point.")]
+	private float m_fNoPlayerRadius;
+
+	[Attribute("0", UIWidgets.EditBox, "Determines how close a player looking at the spawn point has to be to disable it.")]
+	private float m_fNoSightRadius;
+
+	[Attribute("US", UIWidgets.EditBox, "Determines which faction can spawn on this spawn point.")]
+	private string m_sFaction;
+
+	[Attribute("0")]
+	protected bool m_bShowInDeployMapOnly;
+
+	[Attribute()]
+	protected ref SCR_UIInfo m_Info;
+	//-----------------------------------------------------------------------
+
+	protected ref Resource m_pResource;
 
 	protected SCR_SpawnPoint m_pSpawnpoint;
 
-	protected bool m_bFirstRun = true;
-
 	override bool Deploy()
 	{
-		if (m_bFirstRun)
-		{
-			TryFindSpawnpoint();
-			m_bFirstRun = false;
-		}
-
-		if (!m_pSpawnpoint)
+		// No resource loaded, report a failure to deploy
+		if (!m_pResource || !m_pResource.IsValid())
 			return false;
 
-		m_pSpawnpoint.SetFactionKey(m_sFactionKey);
-		return true;
+		// Spawn params
+		EntitySpawnParams params = new EntitySpawnParams();
+		params.TransformMode = ETransformMode.WORLD;
+		GetAttachedEntity().GetTransform(params.Transform);
+		params.Transform[3] = params.Transform[3] + GetAttachedEntity().VectorToParent(m_vSpawnpointOffset);
+
+		// Do spawn
+		m_pSpawnpoint = SCR_SpawnPoint.Cast(GetGame().SpawnEntityPrefab(m_pResource, GetAttachedEntity().GetWorld(), params));
+
+		// Return if successful
+		return m_pSpawnpoint;
 	}
 
 	override bool Undeploy()
@@ -28,24 +54,38 @@ class NO_SCR_SpawnpointDeployment : NO_SCR_DeploymentInterface
 		if (!m_pSpawnpoint)
 			return false;
 
-		m_pSpawnpoint.SetFactionKey(string.Empty);
+		SCR_EntityHelper.DeleteEntityAndChildren(m_pSpawnpoint);
 		return true;
 	}
 
-	protected void TryFindSpawnpoint()
+	protected void LoadAndModifySpawnResource()
 	{
-		IEntity child = GetAttachedEntity().GetChildren();
-		while (child)
+		// Load the resource
+		m_pResource = Resource.Load("{E7F4D5562F48DDE4}Prefabs/MP/Spawning/SpawnPoint_Base.et");
+		if (!m_pResource)
+			return;
+
+		// Modify the resource with the settings of this deployment
+		BaseContainer baseContainer = m_pResource.GetResource().ToBaseContainer();
+		if (baseContainer)
 		{
-			SCR_SpawnPoint spawnpoint = SCR_SpawnPoint.Cast(child);
-
-			if (spawnpoint)
-			{
-				m_pSpawnpoint = spawnpoint;
-				return;
-			}
-
-			child = child.GetSibling();
+			baseContainer.Set("m_fSpawnRadius", m_fSpawnRadius);
+			baseContainer.Set("m_fNoPlayerRadius", m_fNoPlayerRadius);
+			baseContainer.Set("m_fNoSightRadius", m_fNoSightRadius);
+			baseContainer.Set("m_sFaction", m_sFaction);
+			baseContainer.Set("m_bShowInDeployMapOnly", m_bShowInDeployMapOnly);
+			baseContainer.Set("m_Info", m_Info);
 		}
+	}
+
+	void NO_SCR_SpawnpointDeployment()
+	{
+		LoadAndModifySpawnResource();
+	}
+
+	void ~NO_SCR_SpawnpointDeployment()
+	{
+		if (m_pResource && m_pResource.IsValid())
+			m_pResource.GetResource().Release();
 	}
 }
